54d420fd0a2f693761f7b820b6421d75172b51b8
georgyshkliaryk <h.shkliaryk@gmail.com>
2021-10-01 01:01:58 +0300

    init

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true }, () => {
+    console.log("Connected to MongoDB!");
+});
+
+//middlewares
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
+
+app.listen(8080, () => {
+    console.log("Server is running...");
+});
---
+++ routes/users.js
@@ -0,0 +1,14 @@
+const router = require("express").Router();
+const User = require("../models/User");
+
+//update user
+
+//delete user
+
+//get a user
+
+//follow a user
+
+//unfollow a user
+
+module.exports = router;
